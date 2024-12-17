6f8de4e467ca6349a94faa53493a983c8f1c9e78
graykr <graykr@users.noreply.github.com>
2024-12-16 18:14:38 -0600

    add tool to ala_redo theme files to convert html to markdown

---
+++ themes/ala_redo/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ac76a36856cd7aa2397e35dbff43421177cd0608',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ac76a36856cd7aa2397e35dbff43421177cd0608',
+            'dev_requirement' => false,
+        ),
+        'league/html-to-markdown' => array(
+            'pretty_version' => '2.2.2',
+            'version' => '2.2.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../league/html-to-markdown',
+            'aliases' => array(),
+            'reference' => '08491df8e9475b219659400edf68416d8044a9db',
+            'dev_requirement' => false,
+        ),
+    ),
+);
